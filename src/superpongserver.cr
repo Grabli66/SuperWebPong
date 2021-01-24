bb0c311c5713a9d8332cb3979756f8b4eb8cc765
Grabli66 <grabli66@gmail.com>
2021-01-24 20:59:24 +0300

    Fix path for heroku

--- src/superpongserver.cr
+++ src/superpongserver.cr
@@ -3,14 +3,14 @@ require "kemal"
 port = (ENV["PORT"]? || 8080).to_i
 
 get "/" do |env|
-  #send_file env, "./html5/bin/index.html"
-  (Dir.entries ".").to_s
+  send_file env, "../bin/html5/bin/index.html"
+  #(Dir.entries ".").to_s
 end
 
 ws "/socket" do |socket|
   socket.send "Hello from Kemal!"
 end
 
-public_folder "html5/bin"
+public_folder "../bin/html5/bin"
 
 Kemal.run port
