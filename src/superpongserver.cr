38a4808a083d4bbd01f5af19153b917cf44394e3
Grabli66 <grabli66@gmail.com>
2021-01-24 20:49:54 +0300

    Test for heroku

--- src/superpongserver.cr
+++ src/superpongserver.cr
@@ -3,7 +3,8 @@ require "kemal"
 port = (ENV["PORT"]? || 8080).to_i
 
 get "/" do |env|
-  send_file env, "./html5/bin/index.html"
+  #send_file env, "./html5/bin/index.html"
+  (Dir.entries ".").to_s
 end
 
 ws "/socket" do |socket|
